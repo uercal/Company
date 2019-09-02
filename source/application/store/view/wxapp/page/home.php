88938ce9ddd8ca427fcdbbdea1d787420423510a
uercal <317215138@qq.com>
2019-09-02 11:14:19 +0800

    Signed-off-by: uercal <317215138@qq.com>

--- source/application/store/view/wxapp/page/home.php
+++ source/application/store/view/wxapp/page/home.php
@@ -21,7 +21,7 @@
                     </div>
                     <!-- 工具栏 -->
                     <div id="diy-menu" class="diy-menu">
-                        <div class="navs">
+                        <!-- <div class="navs">
                             <div id="">
                                 <div class="title">组件</div>
                                 <div id="components">
@@ -33,7 +33,7 @@
                                     <nav class="special" data-type="company"> 友情链接</nav>
                                 </div>
                             </div>
-                        </div>
+                        </div> -->
                         <div class="action">
                             <a id="back-top" class="am-fl am-btn am-btn-xs am-btn-default" href="javascript:;">
                                 <span class="am-icon-angle-double-up"></span> 返回顶部
